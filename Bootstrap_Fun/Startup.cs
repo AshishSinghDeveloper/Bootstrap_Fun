097e1b65c2f7f877ed5ea7e00dc962daa5bde042
Ashish Singh <ashishsng21@hotmail.com>
2021-01-07 20:00:16 -0600

    Remove Bootstrap_fun project

--- Bootstrap_Fun/Startup.cs
+++ Bootstrap_Fun/Startup.cs
@@ -16,6 +16,7 @@ namespace Bootstrap_Fun
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +28,8 @@ namespace Bootstrap_Fun
             }
 
             app.UseRouting();
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
